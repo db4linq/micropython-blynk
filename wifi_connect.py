0fc69a1fd26d645ef6e0b16b697c981df73f6918
db4linq <db4linq@gmail.com>
2017-09-18 15:46:58 +0700

    add wifi

---
+++ wifi_connect.py
@@ -0,0 +1,1 @@
+import networkwlan=Nonedef connect():  global wlan  wlan=network.WLAN(network.STA_IF)   wlan.active(True)  wlan.connect('<SSID>', '<PASSWORD>')  while not wlan.isconnected():    pass  print('IPAddress: {}'.format(wlan.ifconfig()[0]))  def get_ip():  return wlan.ifconfig()[0]
